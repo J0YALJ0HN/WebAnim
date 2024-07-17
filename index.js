b3529f061312e7d4f292148575e54af4b3eb8165
J0YALJ0HN <JoyalJohn3500@gmail.com>
2024-07-17 18:36:28 +0100

    fixed animation render.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const track = document.getElementById("image-track");
 
+let mouseDownAt = 0;
+let prevPercentage = 0;
+
+track.onmousedown = e => {
+    mouseDownAt = e.clientX;
+};
+
 window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-}
+    mouseDownAt = 0;
+    prevPercentage = parseFloat(track.dataset.percentage) || 0;
+};
 
-window.onmousedown = e => {
-    if(track.dataset.mouseDownAt === "0") return;
+window.onmousemove = e => {
+    if (mouseDownAt === 0) return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX.clientX
-    maxDelta = window.innerWidth / 2;
+    const mouseDelta = mouseDownAt - e.clientX;
+    const maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * -100,
-          nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    const percentage = (mouseDelta / maxDelta) * -50;
+    const nextPercentage = prevPercentage + percentage;
 
     track.dataset.percentage = nextPercentage;
 
-    track.style.transform = 'translate(${nextPercentage}%, -50%)'
-}
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    for (const image of track.getElementsByClassName("image")) {
+        image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+};
